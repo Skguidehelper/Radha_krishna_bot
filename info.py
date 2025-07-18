34c7b059a2e46bffe0f825ac0f647cece6d1b891
Skguidehelper <skguidehelper@gmail.com>
2025-07-18 21:39:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fofete7368:dPbnkuAeaUW7KdOp@alexa.gylmvja.mongodb.net/?retryWrites=true&w=majority&appName=Alexa")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "fofete7368")
+DATABASE_URI = environ.get('DATABASE_URI', "")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
